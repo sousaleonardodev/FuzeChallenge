76f66aaaf6853bfbd5abb85ad6d4778dd6501714
Leonardo Sousa <sousaleonardo@outlook.com>
2025-03-14 12:19:29 -0300

    feat: Create MatchListViewModel class

---
+++ FuzeChallenge/FuzeChallenge/Modules/MatchesList/MatchListViewModel.swift
@@ -0,0 +1,39 @@
+class MatchListViewModel: ObservableObject, Identifiable {
+	@Published var datasource: [MatchViewModel] = []
+	private var cancellables: Set<AnyCancellable> = []
+
+	private let matchFetcher: MatchFeachable
+
+	init(matchFetcher: MatchFeachable) {
+		self.matchFetcher = matchFetcher
+		self.fetchMatches()
+	}
+
+	func fetchMatches() {
+		matchFetcher.listMatches()
+			.map { response in
+				response.map(MatchViewModel.init)
+			}
+			.receive(on: DispatchQueue.main)
+			.sink { [weak self] value in
+				guard let self = self else {
+					return
+				}
+
+				switch value {
+				case .failure(let error):
+					self.datasource = []
+					print(error.localizedDescription)
+				case .finished:
+					break
+				}
+			} receiveValue: { [weak self] matches in
+				guard let self = self else {
+					return
+				}
+
+				self.datasource = matches
+			}
+			.store(in: &cancellables)
+	}
+}
