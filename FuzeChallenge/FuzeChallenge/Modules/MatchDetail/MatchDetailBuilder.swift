c41d2f4808d2f6b837f9d2e9649da8cdc489045c
Leonardo Sousa <sousaleonardo@outlook.com>
2025-03-16 22:26:57 -0300

    feat: Create build to Match detail

---
+++ FuzeChallenge/FuzeChallenge/Modules/MatchDetail/MatchDetailBuilder.swift
@@ -0,0 +1,15 @@
+//
+
+import SwiftUI
+
+struct MatchDetailBuilder {
+	static func build() -> some View {
+		return MatchDetailView()
+	}
+}
+
+struct MatchDetailView: View {
+	var body: some View {
+		Text("Match Detail View")
+	}
+}
